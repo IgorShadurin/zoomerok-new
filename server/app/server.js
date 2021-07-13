f7d307f7f87ccb51e3a0977a91f2d1ed54732e84
Igor Shadurin <igor.shadurin@gmail.com>
2021-07-13 17:16:20 +0300

    validate env

--- server/app/server.js
+++ server/app/server.js
@@ -1,4 +1,26 @@
-const app = require("./app");
-
+const app = require('./app');
+const fs = require('fs');
+require('dotenv').config();
 const port = process.env.PORT || 8080;
+
+function validateEnv() {
+    const staticPath = process.env.APP_STATIC_VIDEO_PATH;
+    if (!(staticPath && fs.existsSync(staticPath))) {
+        throw new Error('Video path not exists');
+    }
+
+    try {
+        const testFile = `${staticPath}/test.txt`;
+        if (fs.existsSync(testFile)) {
+            fs.rmSync(testFile);
+        }
+
+        fs.writeFileSync(testFile, 'Some data');
+        fs.rmSync(testFile);
+    } catch (e) {
+        throw new Error(`Static dir is not writable? ${e.message}`);
+    }
+}
+
+validateEnv();
 app.listen(port, () => console.log(`Started server at http://localhost:${port}!`));
